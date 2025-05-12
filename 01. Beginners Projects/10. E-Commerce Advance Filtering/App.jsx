5964bfbab172ad7d65b7264d2d446f9cedce331d
Jeffrey <netteyjeff@gmail.com>
2025-05-12 16:43:47 +0000

    Beginners E-Commerce Advanced filtering project update

--- 01. Beginners Projects/10. E-Commerce Advance Filtering/App.jsx
+++ 01. Beginners Projects/10. E-Commerce Advance Filtering/App.jsx
@@ -1,13 +1,15 @@
 import Nav from './Navigation/Nav'
 import Products from './Products/Products'
 import Recommended from './Recommended/Recommended'
+import Sidebar from './Sidebar/Sidebar'
 
 const App = () => {
   return (
     <>
+        <Sidebar />
         <Nav />
-        <Products />
         <Recommended />
+        <Products />
     </>
   )
 }
--- 01. Beginners Projects/10. E-Commerce Advance Filtering/components/Card.jsx
+++ 01. Beginners Projects/10. E-Commerce Advance Filtering/components/Card.jsx
@@ -1,7 +1,33 @@
 const Card = () => {
   return (
-    <div>Card</div>
-  )
-}
+    <section className="card">
+      <img
+        src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg"
+        alt="Shoe"
+        className="card-img"
+      />
+      <div className="card-details">
+        <h3 className="card-title">Shoe</h3>
+        <section className="card-reviews">
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <AiFillStar className="rating-star" />
+          <span className="total-reviews">4</span>
+        </section>
 
-export default Card
+        <section className="card-price">
+          <div className="price">
+            <del>$300</del>200
+          </div>
+
+          <div className="bag">
+            <BsBagHeartFill className="bag-icon" />
+          </div>
+        </section>
+      </div>
+    </section>
+  );
+};
+
+export default Card;
